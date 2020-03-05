438d104b50dd6d1d517f1b772336a51a8bc081d5
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-03-05 17:20:34 +0700

    hoang

--- bash.sh
+++ bash.sh
@@ -1,16 +1,38 @@
 #!/usr/bin/env bash
 set -e
+export PROJPATH=$pwd
+echo $PROJPATH
 for row in $(jq -c '.[]' version.json); do
   KEY=$(echo "${row}" | jq -r '.KEY')
   VALUE=$(echo "${row}" | jq -r '.VALUE')
   TYPE=$(echo "${row}" | jq -r '.TYPE')
   echo $KEY $VALUE $TYPE
-  if [[ $TYPE == "Custom" ]]; then
-    echo "type Custom"
-  if [[ $TYPE == "Secret" ]]; then
-    echo "type Secret"
-  if [[ $TYPE == "Configmap" ]]; then
-    echo "type Configmap"
-fi
+    if [[ $TYPE == "Custom" ]]; then
+        for file in $PROJPATH/*; do
+            if [[ -f $file ]]; then
+            cat "$file" \
+            | awk "{gsub(/{IMAGE}/,\"$IMAGE\")}1" \
+            | awk "{gsub(/{NODEENV}/,\"$K8SNS\")}1" \
+            | awk "{gsub(/{NS_PROJECT}/,\"$NS_PROJECT\")}1" 
+            fi
+        done
+    elif [[ $TYPE == "Secret" ]]; then
+        for file in $PROJPATH/*; do
+            if [[ -f $file ]]; then
+            cat "$file" \
+            | awk "{gsub(/{IMAGE}/,\"$IMAGE\")}1" \
+            | awk "{gsub(/{NODEENV}/,\"$K8SNS\")}1" \
+            | awk "{gsub(/{NS_PROJECT}/,\"$NS_PROJECT\")}1" 
+            fi
+        done
+    elif [[ $TYPE == "Configmap" ]]; then
+        for file in $PROJPATH/*; do
+            if [[ -f $file ]]; then
+            cat "$file" \
+            | awk "{gsub(/{IMAGE}/,\"$IMAGE\")}1" \
+            | awk "{gsub(/{NODEENV}/,\"$K8SNS\")}1" \
+            | awk "{gsub(/{NS_PROJECT}/,\"$NS_PROJECT\")}1" 
+            fi
+        done
+    fi
 done
-
