556a7e114fd5ef9705f50290f9267ab416fe1f82
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-03-06 13:33:29 +0700

    hoang

--- bash.sh
+++ bash.sh
@@ -8,23 +8,23 @@ for row in $(jq -c '.key[]' project.json); do
    VALUE=$(echo "${row}" | jq -r '.VALUE')
    TYPE=$(echo "${row}" | jq -r '.TYPE')
     if [[ $TYPE == "Custom" ]]; then
-        echo -e '\t\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t valueFrom: '>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t configMapKeyRef: '>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t\t name: '$ENV_NAME>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t\t key: '$KEY>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t valueFrom: '>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t configMapKeyRef: '>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t\t name: '$ENV_NAME>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t\t key: '$KEY>> deployment.yaml.tmpl
     elif [[ $TYPE == "Secret" ]]; then
-        echo -e '\t\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t valueFrom: '>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t secretKeyRef: '>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t\t name: '$ENV_NAME>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t\t key: '$KEY>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t valueFrom: '>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t secretKeyRef: '>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t\t name: '$ENV_NAME>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t\t key: '$KEY>> deployment.yaml.tmpl
     elif [[ $TYPE == "Configmap" ]]; then
-        echo -e '\t\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t valueFrom: '>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t configMapKeyRef:'>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t\t name: '$ENV_NAME>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\t\t key: '$KEY>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t valueFrom: '>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t configMapKeyRef:'>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t\t name: '$ENV_NAME>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t\t key: '$KEY>> deployment.yaml.tmpl
     fi
 done
 sed -i $'s/\t/  /g' *.yaml.tmpl
