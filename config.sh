6fb2619b8a9a9f35add9ec511c1126bbdc082b82
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-03-06 10:01:08 +0700

    hoang

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo -e 'apiVersion: apps/v1' >>config.yaml
-echo -e '\t metadata:'  >>config.yaml
-echo -e '\t\t namespace:' default >>config.yaml
+echo -e 'apiVersion: apps/v1'>> config.yaml
+echo -e '\t metadata:'>> config.yaml
+echo -e '\t\t namespace: default'>> config.yaml
