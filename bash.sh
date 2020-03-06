705ec63f01576a86d7c9023c5c36226ae6352144
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-03-06 11:06:22 +0700

    hoang

--- bash.sh
+++ bash.sh
@@ -10,33 +10,11 @@ for row in $(jq -c '.key[]' version.json); do
   TYPE=$(echo "${row}" | jq -r '.TYPE')
   echo $KEY $VALUE $TYPE
     if [[ $TYPE == "Custom" ]]; then
-
-        echo '- name: $KEY'>> config.yaml
-        echo '\t valueFrom:'>> config.yaml
-        echo '\t\t configMapKeyRef:'>> config.yaml
-        echo '\t\t\t name: $ENV_NAME'>> config.yaml
-        echo '\t\t\t key: $KEY'>> config.yaml
-        expand -t 1 config.yaml
+        bash custom.sh
     elif [[ $TYPE == "Secret" ]]; then
-        echo "Secret"
-        # for file in $PROJPATH/*; do
-        #     if [[ -f $file ]]; then
-        #     cat "$file" \
-        #     | awk "{gsub(/{IMAGE}/,\"$IMAGE\")}1" \
-        #     | awk "{gsub(/{NODEENV}/,\"$K8SNS\")}1" \
-        #     | awk "{gsub(/{NS_PROJECT}/,\"$NS_PROJECT\")}1" 
-        #     fi
-        # done
+        bash secret.sh
     elif [[ $TYPE == "Configmap" ]]; then
-        echo "Configmap"
-        # for file in $PROJPATH/*; do
-        #     if [[ -f $file ]]; then
-        #     cat "$file" \
-        #     | awk "{gsub(/{IMAGE}/,\"$IMAGE\")}1" \
-        #     | awk "{gsub(/{NODEENV}/,\"$K8SNS\")}1" \
-        #     | awk "{gsub(/{NS_PROJECT}/,\"$NS_PROJECT\")}1" 
-        #     fi
-        # done
+        bash configmap.sh
     fi
 done
 sed -i $'s/\t/ /g' *.yaml.tmpl
