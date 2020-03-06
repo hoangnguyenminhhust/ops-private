467cfb79eb8ac855b3f5b5957d5a4bf842f02be5
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-03-06 11:39:56 +0700

    hoang

--- deployment.sh
+++ deployment.sh
@@ -4,24 +4,24 @@ echo -e 'kind: Deployment'>>deployment.yaml.tmpl
 echo -e 'metadata:'>>deployment.yaml.tmpl
 echo -e '\t name: {NS_PROJECT}'>>deployment.yaml.tmpl
 echo -e 'spec:'>>deployment.yaml.tmpl
-echo -e '\t replicas: 1'>>deployment.yaml.tmpl
-echo -e '\t selector:'>>deployment.yaml.tmpl
-echo -e '\t\t matchLabels:'>>deployment.yaml.tmpl
-echo -e '\t\t\t app: {NS_PROJECT}'>>deployment.yaml.tmpl
-echo -e '\t template:'>>deployment.yaml.tmpl
-echo -e '\t\t metadata:'>>deployment.yaml.tmpl
-echo -e '\t\t\t labels:'>>deployment.yaml.tmpl
-echo -e '\t\t\t\t app: {NS_PROJECT}'>>deployment.yaml.tmpl
-echo -e '\t\t spec:'>>deployment.yaml.tmpl
-echo -e '\t\t\t containers:'>>deployment.yaml.tmpl
-echo -e '\t\t\t\t - name: {NS_PROJECT}'>>deployment.yaml.tmpl
-echo -e '\t\t\t\t\t image: {IMAGE}'>>deployment.yaml.tmpl
-echo -e '\t\t\t\t\t resources:'>>deployment.yaml.tmpl
-echo -e '\r\r\t\t\t\t requests:'>>deployment.yaml.tmpl
-echo -e '\t\t\t\t\t\t\t cpu: 10m'>>deployment.yaml.tmpl
-echo -e '\t\t\t\t\t ports:'>>deployment.yaml.tmpl
-echo -e '\t\t\t\t\t  - containerPort: 3000'>>deployment.yaml.tmpl
-echo -e '\t\t\t\t\t env:'>>deployment.yaml.tmpl
+echo -e '\treplicas: 1'>>deployment.yaml.tmpl
+echo -e '\tselector:'>>deployment.yaml.tmpl
+echo -e '\t\tmatchLabels:'>>deployment.yaml.tmpl
+echo -e '\t\t\tapp: {NS_PROJECT}'>>deployment.yaml.tmpl
+echo -e '\ttemplate:'>>deployment.yaml.tmpl
+echo -e '\t\tmetadata:'>>deployment.yaml.tmpl
+echo -e '\t\t\tlabels:'>>deployment.yaml.tmpl
+echo -e '\t\t\t\tapp: {NS_PROJECT}'>>deployment.yaml.tmpl
+echo -e '\t\tspec:'>>deployment.yaml.tmpl
+echo -e '\t\t\tcontainers:'>>deployment.yaml.tmpl
+echo -e '\t\t\t\t- name: {NS_PROJECT}'>>deployment.yaml.tmpl
+echo -e '\t\t\t\t\timage: {IMAGE}'>>deployment.yaml.tmpl
+echo -e '\t\t\t\t\tresources:'>>deployment.yaml.tmpl
+echo -e '\r\r\t\t\t\trequests:'>>deployment.yaml.tmpl
+echo -e '\t\t\t\t\t\t\tcpu: 10m'>>deployment.yaml.tmpl
+echo -e '\t\t\t\t\tports:'>>deployment.yaml.tmpl
+echo -e '\t\t\t\t\t\t- containerPort: 3000'>>deployment.yaml.tmpl
+echo -e '\t\t\t\t\tenv:'>>deployment.yaml.tmpl
 
 
 
