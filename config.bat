ceea2e59025b04a16aa26cc438cc7c9b6887f8ab
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-03-06 09:53:36 +0700

    hoang

--- config.bat
+++ config.bat
@@ -1,4 +1,4 @@
 #!/bin/bash
-printf  "apiVersion: apps/v1" >>config.yaml
-printf  "\t metadata:"  >>config.yaml
-printf  "\t\t namespace:" default >>config.yaml
+echo -r "apiVersion: apps/v1" >>config.yaml
+echo -r "\t metadata:"  >>config.yaml
+echo -r "\t\t namespace:" default >>config.yaml
