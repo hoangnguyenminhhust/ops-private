9d8b3eca9298c92ff841ec13719830e8641f047e
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-03-12 11:14:02 +0700

    hoang

--- bash.sh
+++ bash.sh
@@ -2,13 +2,14 @@
 set -e
 # curl -o project.json http://nsd-api.elove.mobi
 export PROJECT_NAME=$(jq -r '.name_project' project.json)
-bash deployment.sh
+    bash deployment.sh
+if [[ $PROJECT_NAME =~ 'api-gateway' ]]; then
     bash service.sh
-fi
+
 for row in $(jq -c '.key[]' project.json); do
-    KEY=$(echo "${row}" | jq -r '.KEY')
-    VALUE=$(echo "${row}" | jq -r '.VALUE')
-    TYPE=$(echo "${row}" | jq -r '.TYPE')
+    KEY=$(echo "${row}" | jq -r '.key')
+    VALUE=$(echo "${row}" | jq -r '.value')
+    TYPE=$(echo "${row}" | jq -r '.type')
     K8S_NAME_KEY=$(echo "${row}" | jq -r '.k8s_name_key')
     if [[ $TYPE == "Custom" ]]; then
         echo -e '\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
