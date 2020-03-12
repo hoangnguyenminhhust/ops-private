82f9542d13501acb6e3ab67d7ba90d18347514d1
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-03-12 11:08:59 +0700

    hoag

--- bash.sh
+++ bash.sh
@@ -3,30 +3,27 @@ set -e
 # curl -o project.json http://nsd-api.elove.mobi
 export PROJECT_NAME=$(jq -r '.name_project' project.json)
 bash deployment.sh
-if [[$PROJECT_NAME =~ 'api-gateway']]; then
     bash service.sh
 fi
 for row in $(jq -c '.key[]' project.json); do
     KEY=$(echo "${row}" | jq -r '.KEY')
     VALUE=$(echo "${row}" | jq -r '.VALUE')
     TYPE=$(echo "${row}" | jq -r '.TYPE')
+    K8S_NAME_KEY=$(echo "${row}" | jq -r '.k8s_name_key')
     if [[ $TYPE == "Custom" ]]; then
         echo -e '\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\tvalueFrom: '>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\tconfigMapKeyRef: '>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\tname: '$PROJECT_NAME>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\tkey: '$KEY>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\tvalue: '$VALUE>> deployment.yaml.tmpl
         elif [[ $TYPE == "Secret" ]]; then
         echo -e '\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
         echo -e '\t\t\t\t\t\t\tvalueFrom: '>> deployment.yaml.tmpl
         echo -e '\t\t\t\t\t\t\t\tsecretKeyRef: '>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\tname: '$PROJECT_NAME>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t\tname: '$K8S_NAME_KEY>> deployment.yaml.tmpl
         echo -e '\t\t\t\t\t\t\t\t\tkey: '$KEY>> deployment.yaml.tmpl
         elif [[ $TYPE == "Configmap" ]]; then
         echo -e '\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
         echo -e '\t\t\t\t\t\t\tvalueFrom: '>> deployment.yaml.tmpl
         echo -e '\t\t\t\t\t\t\t\tconfigMapKeyRef:'>> deployment.yaml.tmpl
-        echo -e '\t\t\t\t\t\t\t\t\tname: '$PROJECT_NAME>> deployment.yaml.tmpl
+        echo -e '\t\t\t\t\t\t\t\t\tname: '$K8S_NAME_KEY>> deployment.yaml.tmpl
         echo -e '\t\t\t\t\t\t\t\t\tkey: '$KEY>> deployment.yaml.tmpl
     fi
 done
