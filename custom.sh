228b12613d34ecee81b037da167adfb714fc67ef
Nguyễn Minh Hoàng <hoangnguyenminh.hust@gmail.com>
2020-03-19 10:10:39 +0700

    hoang

--- custom.sh
+++ custom.sh
@@ -5,4 +5,4 @@ KEY=$1
 VALUE=$2
 
 echo -e '\t\t\t\t\t\t- name: '$KEY>> deployment.yaml.tmpl
-echo -e '\t\t\t\t\t\t\tvalue: '$VALUE>> deployment.yaml.tmpl
+echo -e '\t\t\t\t\t\t\tvalue: '"$VALUE">> deployment.yaml.tmpl
