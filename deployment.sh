b23871aa9eb913a2bab6795011763f402776eb4c
Nguyễn Minh Hoàng <hoangnguyenminh.hust@gmail.com>
2020-03-19 10:15:34 +0700

    hoang

--- deployment.sh
+++ deployment.sh
@@ -4,30 +4,30 @@ set -e
 NAME_PROJECT=$1
 REPLICAS=$2
 
-echo -e '---'> deployment.yaml.tmpl
-echo -e 'apiVersion: apps/v1'>> deployment.yaml.tmpl
-echo -e 'kind: Deployment'>> deployment.yaml.tmpl
-echo -e 'metadata:'>> deployment.yaml.tmpl
-echo -e '\tname: '$NAME_PROJECT>> deployment.yaml.tmpl
-echo -e 'spec:'>> deployment.yaml.tmpl
-echo -e '\treplicas: '$REPLICAS>> deployment.yaml.tmpl
-echo -e '\tselector:'>> deployment.yaml.tmpl
-echo -e '\t\tmatchLabels:'>> deployment.yaml.tmpl
-echo -e '\t\t\tapp: '$NAME_PROJECT>> deployment.yaml.tmpl
-echo -e '\ttemplate:'>> deployment.yaml.tmpl
-echo -e '\t\tmetadata:'>> deployment.yaml.tmpl
-echo -e '\t\t\tlabels:'>> deployment.yaml.tmpl
-echo -e '\t\t\t\tapp: '$NAME_PROJECT>> deployment.yaml.tmpl
-echo -e '\t\tspec:'>> deployment.yaml.tmpl
-echo -e '\t\t\tcontainers:'>> deployment.yaml.tmpl
-echo -e '\t\t\t\t- name: '$NAME_PROJECT>> deployment.yaml.tmpl
-echo -e '\t\t\t\t\timage: {IMAGE}'>> deployment.yaml.tmpl
-echo -e '\t\t\t\t\tresources:'>> deployment.yaml.tmpl
-echo -e '\t\t\t\t\t\trequests:'>> deployment.yaml.tmpl
-echo -e '\t\t\t\t\t\t\tcpu: 10m'>> deployment.yaml.tmpl
-echo -e '\t\t\t\t\tports:'>> deployment.yaml.tmpl
-echo -e '\t\t\t\t\t\t- containerPort: 3000'>> deployment.yaml.tmpl
-echo -e '\t\t\t\t\tenv:'>> deployment.yaml.tmpl
+echo -e '---'> deployment.yaml
+echo -e 'apiVersion: apps/v1'>> deployment.yaml
+echo -e 'kind: Deployment'>> deployment.yaml
+echo -e 'metadata:'>> deployment.yaml
+echo -e '\tname: '$NAME_PROJECT>> deployment.yaml
+echo -e 'spec:'>> deployment.yaml
+echo -e '\treplicas: '$REPLICAS>> deployment.yaml
+echo -e '\tselector:'>> deployment.yaml
+echo -e '\t\tmatchLabels:'>> deployment.yaml
+echo -e '\t\t\tapp: '$NAME_PROJECT>> deployment.yaml
+echo -e '\ttemplate:'>> deployment.yaml
+echo -e '\t\tmetadata:'>> deployment.yaml
+echo -e '\t\t\tlabels:'>> deployment.yaml
+echo -e '\t\t\t\tapp: '$NAME_PROJECT>> deployment.yaml
+echo -e '\t\tspec:'>> deployment.yaml
+echo -e '\t\t\tcontainers:'>> deployment.yaml
+echo -e '\t\t\t\t- name: '$NAME_PROJECT>> deployment.yaml
+echo -e '\t\t\t\t\timage: {IMAGE}'>> deployment.yaml
+echo -e '\t\t\t\t\tresources:'>> deployment.yaml
+echo -e '\t\t\t\t\t\trequests:'>> deployment.yaml
+echo -e '\t\t\t\t\t\t\tcpu: 10m'>> deployment.yaml
+echo -e '\t\t\t\t\tports:'>> deployment.yaml
+echo -e '\t\t\t\t\t\t- containerPort: 3000'>> deployment.yaml
+echo -e '\t\t\t\t\tenv:'>> deployment.yaml
 
 
 
